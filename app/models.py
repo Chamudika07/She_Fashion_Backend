ca6c2b7c8c6ce2a1208a935aa78a07acd8dd88f1
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-05 19:46:48 +0530

    add new table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String , ForeignKey
+from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String , ForeignKey , Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import null , text
 from .database import Base
@@ -19,5 +19,31 @@ class SchoolShoes(Base):
     size = Column(Integer, nullable=False)
     type = Column(String, nullable=False)
     boysORgirls = Column(String, nullable=False)
+    quntity = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=text('now()'))
+    
+class SchoolShirts(Base):
+    __tablename__ = "school_shirts"
+
+    id = Column(Integer, primary_key=True , nullable=False)
+    size = Column(Float, nullable=False)
+    quntity = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=text('now()'))
+
+class SchoolShorts(Base):
+    __tablename__ = "school_shorts"
+
+    id = Column(Integer, primary_key=True , nullable=False)
+    size = Column(Integer, nullable=False)
+    color = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    quntity = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=text('now()'))
+    
+class SchoolFrocks(Base):
+    __tablename__ = "school_frocks"
+
+    id = Column(Integer, primary_key=True , nullable=False)
+    size = Column(String, nullable=False)
     quntity = Column(Integer, nullable=False)
     created_at = Column(TIMESTAMP, nullable=False, server_default=text('now()'))
